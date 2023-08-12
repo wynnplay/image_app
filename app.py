39304acdb46b827701a943d63b2671433dd76b66
wynnplay <141928525+wynnplay@users.noreply.github.com>
2023-08-12 15:31:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ if uploaded_file is not None:
     input = Image.open(uploaded_file)
     st.image(input, caption='원본 이미지', use_column_width=True)
     output = remove(input)
-    st.image(output, caption='배경 제거 이미지', use_column_width=True) 
+    st.image(output, caption='배경 제거 이미지', use_column_width=True)
