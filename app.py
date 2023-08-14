d26399ca0000333e9d6c12aebb5ca1efb42efb74
wynnplay <141928525+wynnplay@users.noreply.github.com>
2023-08-14 17:34:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 
 st.set_page_config(page_title='My_First_Image_App', layout="centered")
 st.subheader('[미니프로젝트] 이미지 배경제거 서비스 + 글자추출 웹서비스')
-st.markdown('### : arrow_forward::sunglasses: Remove Background - `rmbg`'
+st.markdown('### : arrow_forward::sunglasses: Remove Background - `rmbg'
 st.markdown("#### sample result") 
 
 image-comparison(
