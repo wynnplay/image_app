8c7df8a7fbfd1b7225bb5499527e6dfcc64527cf
wynnplay <141928525+wynnplay@users.noreply.github.com>
2023-08-14 17:06:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,25 @@ import streamlit as st
 from rembg import remove
 from PIL import Image
 
-st.subheader('이미지 배경제거 서비스')
+from streamlit-image-comparison import image-comparison
+import easyocr as ocr #ocr
+import numpy as np  
+
+st.set_page_config(page_title='My_First_Image_App', layout="centered")
+st.subheader('[미니프로젝트] 이미지 배경제거 서비스 + 글자추출 웹서비스')
+st.markdown('### : arrow_forward::sunglasses: Remove Background - `rmbg`'
+st.markdown('#### sample result') 
+
+image-comparison(
+    img1 = "https://   ", 
+    img2 = "https://   ",
+    label1 = "원본 이미지", 
+    label2 = '배경제거 이미지", 
+    show_labels = True, 
+    make-responsive=True, 
+    in_memory=True)  
+    
+st.markdown('### :arrow_forward: :ab:글자 추출(OCR) - `easyocr`')  
 
 uploaded_file = st.file_uploader("이미지를 업로드하세요", type=['png', 'jpg', 'jpeg'])
 
